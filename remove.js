7b8a5ed6efe338df0f278681ff5c0830c173f6a9
Fernando Valani Cavalcante <valanife@gmail.com>
2021-06-08 14:47:32 -0300

    remove complete

--- remove.js
+++
@@ -1,5 +0,0 @@
-import readlineSync from "readline-sync";
-
-export default function remove() {
-  return;
-}
--- terminalTodo.js
+++ terminalTodo.js
@@ -1,21 +1,66 @@
 import readlineSync from "readline-sync";
-import add from "./add.js";
-import list from "./list.js";
-import remove from "./remove.js";
 
 const actions = ["Add", "List", "Remove"];
+const actionsCheck = ["Add", "List", "Check", "Remove"];
 let index = readlineSync.keyInSelect(actions, "Type your command: ");
 const todoList = [];
 
 while (index !== -1) {
-  if (index === 0) {
-    todoList.push(readlineSync.question("What do you to do? "));
-  } else if (index === 1) {
-    console.log("========================");
-    todoList.forEach((item) => console.log(item));
-    console.log("========================");
-  } else if (index === 2) {
-    remove();
+  if (todoList.length === 0) {
+    if (index === 0) {
+      todoList.push({
+        done: "🔴",
+        name: `${readlineSync.question("What do you to do? ")}`,
+      });
+    } else if (index === 1) {
+      console.log("========================");
+      todoList.forEach((item) => `${console.log(item.done, item.name)}}`);
+      console.log("========================");
+    } else if (index === 2) {
+      const remove = readlineSync.keyInSelect(
+        todoList.map((item) => item.done + " " + item.name),
+        "What todo do you want to remove? "
+      );
+      todoList.splice(remove, 1);
+      console.log("========================");
+      todoList.forEach((item) => `${console.log(item.done, item.name)}}`);
+      console.log("========================");
+    }
+  } else {
+    if (index === 0) {
+      todoList.push({
+        done: "🔴",
+        name: `${readlineSync.question("What do you to do? ")}`,
+      });
+    } else if (index === 1) {
+      console.log("========================");
+      todoList.forEach((item) => `${console.log(item.done, item.name)}}`);
+      console.log("========================");
+    } else if (index === 2) {
+      const check = readlineSync.keyInSelect(
+        todoList.map((item) => item.done + " " + item.name),
+        "What todo do you want to check/uncheck? "
+      );
+      if (todoList[check].done === "🟢") {
+        todoList[check].done = "🔴";
+      } else {
+        todoList[check].done = "🟢";
+      }
+    } else if (index === 3) {
+      const remove = readlineSync.keyInSelect(
+        todoList.map((item) => item.done + " " + item.name),
+        "What todo do you want to remove? "
+      );
+
+      todoList.splice(remove, 1);
+      console.log("========================");
+      todoList.forEach((item) => `${console.log(item.done, item.name)}}`);
+      console.log("========================");
+    }
   }
-  index = readlineSync.keyInSelect(actions, "Type your command: ");
+
+  index = readlineSync.keyInSelect(
+    todoList.length === 0 ? actions : actionsCheck,
+    "Type your command: "
+  );
 }
